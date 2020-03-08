3f0b41874ca927f56b202225e066e5fcc338e513
Sungdoolim <sungdoolim@naver.com>
2020-03-08 19:49:41 +0900

    spring - android

--- mysqltest/app/src/main/java/com/example/mysqltest/Main2Activity.java
+++ mysqltest/app/src/main/java/com/example/mysqltest/Main2Activity.java
@@ -11,9 +11,11 @@ import android.widget.ListView;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.conn.ClientConnectionManager;
@@ -21,6 +23,7 @@ import org.apache.http.impl.client.DefaultBackoffStrategy;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.HttpParams;
 import org.apache.http.protocol.HttpContext;
 import org.apache.http.util.EntityUtils;
@@ -57,17 +60,22 @@ public class Main2Activity extends AppCompatActivity implements Runnable {
 
             // http client 객체/
              HttpClient http = new DefaultHttpClient();
-
-
             //HttpClient http = HttpClientBuilder.create().build();
 
-
-
+            // NameValuePair : 변수명과 값을 함께 저장하는 객체로 제공되는 객체이다.
+            ArrayList<NameValuePair> postData = new ArrayList<>();
+// post 방식으로 전달할 값들을 postData 객체에 집어 넣는다.
+            postData.add(new BasicNameValuePair("id","id 입니다"));
+            postData.add(new BasicNameValuePair("pw","pw 입니다"));   // pw라는 스트링에 패스워드 값을 넣어서 포스트로 전달!!!!!!!!!
+// url encoding이 필요한 값들(한글, 특수문자) : 한글은 인코딩안해주면 깨짐으로 인코딩을 한다.
+            UrlEncodedFormEntity request = new UrlEncodedFormEntity(postData,"utf-8");
 
             // post 방식으로 전송하는 객체
             HttpPost httpPost = new HttpPost("http://192.168.56.1:8052/controller/androidtest.json");
 
             // http클라이언트.execute(httppost객체) : 웬서버에 데이터를 전달
+            httpPost.setEntity(request);
+// post 방식으로 전송, 응답결과는 response로 넘어옴
             // 결과(json)가 response로 넘어옴
             HttpResponse response = http.execute(httpPost);
             // body에 json 스트링이 넘어옴
@@ -75,6 +83,8 @@ public class Main2Activity extends AppCompatActivity implements Runnable {
             System.out.println("#################################################"+body);
             // string을 JSONObject로 변환
             JSONObject jsonObj = new JSONObject(body);
+// 스프링 컨트롤러에서 리턴해줄 때 저장했던 값을 꺼냄
+          //  String message = jsonObj.getString("message");
             // json객체.get("변수명")
             JSONArray jArray = (JSONArray)jsonObj.get("sendData");
             for(int i=0; i<jArray.length();i++){
@@ -104,6 +114,7 @@ public class Main2Activity extends AppCompatActivity implements Runnable {
             for (int i = 0; i < str.length; i++) {
                 SampleVO dto = items.get(i);
                 str[i] = dto.getMno() + "(" + dto.getFirstName() + ")";
+                System.out.println(str[i]);
             }
             ArrayAdapter<String> adapter = new ArrayAdapter<String>(Main2Activity.this, android.R.layout.simple_list_item_1, str);
             // adapter와 data 바인딩
