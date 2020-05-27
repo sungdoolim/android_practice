471e0e793048e67c0697858c768e9e735c375468
staris <sungdoolim@naver.com>
2020-05-28 00:02:29 +0900

    onitemclic evt listvw

--- kotlin_prac/app/src/main/java/com/example/kotlin_prac/Main3Activity.kt
+++ kotlin_prac/app/src/main/java/com/example/kotlin_prac/Main3Activity.kt
@@ -2,7 +2,9 @@ package com.example.kotlin_prac
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main3.*
 
 class Main3Activity : AppCompatActivity() {
@@ -13,14 +15,16 @@ class Main3Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
 //
-//        var item=arrayOf(3,5,"star",'q')
-//        lv.adapter=ArrayAdapter<Any>(this,android.R.layout.simple_list_item_1,item)
+//  var item=arrayOf(3,5,"star",'q')
+//  lv.adapter=ArrayAdapter<Any>(this,android.R.layout.simple_list_item_1,item)
 
         val adpt=customAdpt(this,Userl)
         lv.adapter=adpt
 
-
-
+        lv.onItemClickListener=AdapterView.OnItemClickListener{parent,view,position,id->
+            val selItem=parent.getItemAtPosition(position) as Users
+            Toast.makeText(this,selItem.name,Toast.LENGTH_SHORT).show()
+        }
 
 
 
--- kotlin_prac/app/src/main/java/com/example/kotlin_prac/customAdpt.kt
+++ kotlin_prac/app/src/main/java/com/example/kotlin_prac/customAdpt.kt
@@ -11,7 +11,8 @@ import android.widget.TextView
 class customAdpt(val context: Context,val Userl:ArrayList<Users> ):BaseAdapter(){
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
       //  TODO("Not yet implemented")
-        val view:View =LayoutInflater.from(context).inflate(R.layout.custom_lv ,null)
+        val view =LayoutInflater.from(context).inflate(R.layout.custom_lv ,null)
+
         var profile=Userl[position].profile
         var name=Userl[position].name
         var age=Userl[position].age
@@ -29,7 +30,7 @@ class customAdpt(val context: Context,val Userl:ArrayList<Users> ):BaseAdapter()
 
     override fun getItem(position: Int): Any {
         //TODO("Not yet implemented")
-        return 0
+        return Userl[position]
     }
 
     override fun getItemId(position: Int): Long {
