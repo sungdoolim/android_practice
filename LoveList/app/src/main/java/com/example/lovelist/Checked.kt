64cc6795554db046b1d079caf6b809ae71ef09ae
staris <sungdoolim@naver.com>
2020-08-13 18:12:01 +0900

    L-list ver2.0

--- LoveList/app/src/main/java/com/example/lovelist/Checked.kt
+++ LoveList/app/src/main/java/com/example/lovelist/Checked.kt
@@ -1,11 +1,16 @@
 package com.example.lovelist
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.core.app.ActivityCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_checked.*
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.bottom_nav.*
 
 class Checked : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
@@ -15,6 +20,7 @@ class Checked : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelect
         setContentView(R.layout.activity_checked)
         botnav.getMenu().getItem(2).setChecked(true);
         botnav.setOnNavigationItemSelectedListener(this)
+        selectList(this)
     }
     override fun onBackPressed() {
         var intent= Intent(this, MainActivity::class.java)
@@ -38,5 +44,32 @@ class Checked : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelect
             else->{}
         }
         return true
+    }
+    fun selectList(container: Context){
+        val firestore= FirebaseFirestore.getInstance()
+        firestore?.collection("ourlist").orderBy("index").get()
+            .addOnCompleteListener {
+                if(it.isSuccessful){
+                    var userDTO=ArrayList<list_data>()
+                    for(dc in it.result!!.documents){
+                        dc.toObject(list_data::class.java)?.let { it1 ->
+                            if(it1.ischecked){
+                                userDTO.add(it1)
+                            }
+                    }
+                    }
+
+                    checked_rv.layoutManager=
+                        LinearLayoutManager(container, LinearLayoutManager.VERTICAL,false)
+                    checked_rv.setHasFixedSize(true)
+                    checked_rv.adapter=
+                        list_adapter(
+                            container,
+                            userDTO
+                        )
+                }else{
+                    println("fail")
+                }
+            }
     }
 }
--- LoveList/app/src/main/java/com/example/lovelist/unChecked.kt
+++ LoveList/app/src/main/java/com/example/lovelist/unChecked.kt
@@ -1,10 +1,15 @@
 package com.example.lovelist
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_un_checked.*
 import kotlinx.android.synthetic.main.bottom_nav.*
 
 class unChecked : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
@@ -14,6 +19,7 @@ class unChecked : AppCompatActivity(), BottomNavigationView.OnNavigationItemSele
         setContentView(R.layout.activity_un_checked)
         botnav.getMenu().getItem(1).setChecked(true);
         botnav.setOnNavigationItemSelectedListener(this)
+        selectList(this)
     }
     override fun onBackPressed() {
         var intent= Intent(this, MainActivity::class.java)
@@ -38,5 +44,32 @@ class unChecked : AppCompatActivity(), BottomNavigationView.OnNavigationItemSele
             else->{}
         }
         return true
+    }
+    fun selectList(container: Context){
+        val firestore= FirebaseFirestore.getInstance()
+        firestore?.collection("ourlist").orderBy("index").get()
+            .addOnCompleteListener {
+                if(it.isSuccessful){
+                    var userDTO=ArrayList<list_data>()
+                    for(dc in it.result!!.documents){
+                        dc.toObject(list_data::class.java)?.let { it1 ->
+                            if(!it1.ischecked){
+                                userDTO.add(it1)
+                            }
+                            }
+                    }
+
+                    unchecked_rv.layoutManager=
+                        LinearLayoutManager(container, LinearLayoutManager.VERTICAL,false)
+                    unchecked_rv.setHasFixedSize(true)
+                    unchecked_rv.adapter=
+                        list_adapter(
+                            container,
+                            userDTO
+                        )
+                }else{
+                    println("fail")
+                }
+            }
     }
 }
